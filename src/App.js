32ae12a07fc2c5e892d22f736200e71890ff96e6
JoseGuevara29 <guevarabus29@gmail.com>
2021-03-06 03:00:19 -0500

    Finished mvp. Styling could be improved.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
-import React from 'react';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Character from "./components/Character";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people")
+      .then((response) => {
+        setCharacters(response.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  // const Name = (props) => <div className="name">{props.data.name}</div>;
+  // console.log(data);
+
+  // console.log(characters);
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header" style={{ margin: "20px" }}>
+        Characters
+      </h1>
+      {characters.map((e, index) => (
+        <Character key={index} data={e} />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,88 @@
 // Write your Character component here
+import React, { useEffect, useState } from "react";
+
+import {
+  Card,
+  CardText,
+  CardBody,
+  Button,
+  Collapse,
+  ListGroup,
+  ListGroupItem,
+} from "reactstrap";
+export default function Character(props) {
+  const [collapse, setCollapse] = useState(false);
+  const [status, setStatus] = useState("Closed");
+  const onEntered = () => setStatus("Opened");
+  const onExited = () => setStatus("Closed");
+  const toggle = () => setCollapse(!collapse);
+
+  console.log(props.data);
+  const films = " " + props.data.films;
+
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+          margin: "25px",
+        }}
+      >
+        <h5>{props.data.name}</h5>
+        <Button
+          outline
+          color="secondary"
+          size="sm"
+          onClick={toggle}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            margin: "0px 25px",
+          }}
+        >
+          Toggle
+        </Button>
+      </div>
+
+      <Collapse isOpen={collapse} onEntered={onEntered} onExited={onExited}>
+        {status === "Opened" && (
+          <Card>
+            <CardBody>
+              <CardText>
+                <ListGroup
+                  // horizontal
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    flex: "1",
+                    height: "100%",
+                  }}
+                >
+                  <ListGroupItem>
+                    Birth Year: {props.data.birth_year}
+                  </ListGroupItem>
+                  <ListGroupItem>Gender: {props.data.gender}</ListGroupItem>
+                  <ListGroupItem>
+                    Skin Color: {props.data.skin_color}
+                  </ListGroupItem>
+                  <ListGroupItem>
+                    Films:
+                    {/* {Object.values(props.data.films)} */}
+                    {films}
+                  </ListGroupItem>
+                  <ListGroupItem>
+                    Vehicles: {props.data.vehicles.length}
+                  </ListGroupItem>
+                </ListGroup>
+              </CardText>
+            </CardBody>
+          </Card>
+        )}
+      </Collapse>
+    </div>
+  );
+}
